ddaab5a5af34e4469ea3a4526ff80788c41091f9
orlando-lam <orlando.lam@kellogg.northwestern.edu>
2022-04-16 21:10:40 +0000

    exercises

--- crm/crm.sql
+++ crm/crm.sql
@@ -2,4 +2,39 @@
 -- run of this script
 -- e.g. DROP TABLE IF EXISTS ______;
 
+DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS activities;
+DROP TABLE IF EXISTS salespersons;
+
 -- CREATE TABLES
+CREATE TABLE contacts (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT,
+  phone_number TEXT,
+  company_id INTEGER
+);
+
+CREATE TABLE companies (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  company TEXT
+);
+
+CREATE TABLE activities (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  contact_type TEXT,
+  date_time TEXT,
+  notes TEXT,
+  contact_id INTEGER,
+  salespersons_id INTEGER
+);
+
+CREATE TABLE salespersons (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT,
+  phone_number TEXT
+);
